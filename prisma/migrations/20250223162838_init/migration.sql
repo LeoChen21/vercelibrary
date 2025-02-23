832ea649d01fd05e7e60b7f4474dc40e0c3d82d1
LeoChen21 <leochenkmts@gmail.com>
2025-02-23 11:29:04 -0500

    generated prisma

---
+++ prisma/migrations/20250223162838_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Media" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Media_pkey" PRIMARY KEY ("id")
+);
